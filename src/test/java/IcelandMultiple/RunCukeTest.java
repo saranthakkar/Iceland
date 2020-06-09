bcb463725f31a4290b8656b1cb61d80bd4f8e7b1
saranthakkar <you@example.com>
2020-06-09 12:06:18 +0100

    updated

--- src/test/java/IcelandMultiple/RunCukeTest.java
+++ src/test/java/IcelandMultiple/RunCukeTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/Resource")
+@CucumberOptions(features = "src/test/Resource/CrispsPage.feature")
 
 public class RunCukeTest {
 }
